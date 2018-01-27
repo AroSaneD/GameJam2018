4aca206f0a36cc7cce43bf4828b6a2ad1eeb765c
Sensationxox <sensationxox@yahoo.com>
2018-01-27 14:03:46 +0200

    Created (started) achievement component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,42 +27,6 @@ export class AppComponent implements AfterViewChecked {
   }
 
   ngAfterViewChecked() {
-    //okpt("CSS Particle Effects");
-    function initparticles() {
-      //bubbles();
-      confetti();
-    }
-
-    /*The measurements are ... whack (so to say), for more general text usage I would generate different sized particles for the size of text; consider this pen a POC*/
-
-    //function bubbles() {
-    //  $.each($(".particletext.bubbles"), function () {
-    //    var bubblecount = ($(this).width() / 50) * 10;
-    //    for (var i = 0; i <= bubblecount; i++) {
-    //      var size = ($.rnd(40, 80) / 10);
-    //      $(this).append('<span class="particle" style="top:' + $.rnd(20, 80) + '%; left:' + $.rnd(0, 95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0, 30) / 10) + 's;"></span>');
-    //    }
-    //  });
-    //}
-
-
-    function confetti() {
-      
-      $.each($(".particletext .confetti, .particletext .confetti-r"), function () {
-        var confetticount = 4;
-        for (var i = 0; i <= confetticount; i++) {
-            $(this).append('<span class="particle c' + $.rnd(1, 2) + '" style="top:' + $.rnd(10, 50) + '%; left:' + $.rnd(0, 100) + '%;width:' + $.rnd(6, 8) + 'px; height:' + $.rnd(3, 4) + 'px;animation-delay: ' + ($.rnd(0, 30) / 10) + 's;"></span>');
-        }
-      });
-    }
-    
-    $.rnd = function (m, n) {
-      m = parseInt(m);
-      n = parseInt(n);
-      return Math.floor(Math.random() * (n - m + 1)) + m;
-    }
-
-    initparticles();
     
   }
 
--- src/app/components/achievement/achievement.component.ts
+++ src/app/components/achievement/achievement.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewEncapsulation } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-achievement',
   templateUrl: './achievement.component.html',
-  styleUrls: ['./achievement.component.css']
-})
-export class AchievementComponent implements OnInit {
+  styleUrls: ['./achievement.component.css'],
+  encapsulation: ViewEncapsulation.None
 
+})
+export class AchievementComponent implements OnInit, AfterViewChecked {
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewChecked(): void {
+    //okpt("CSS Particle Effects");
+    function initparticles() {
+      //bubbles();
+      confetti();
+    }
+
+    /*The measurements are ... whack (so to say), for more general text usage I would generate different sized particles for the size of text; consider this pen a POC*/
+
+    //function bubbles() {
+    //  $.each($(".particletext.bubbles"), function () {
+    //    var bubblecount = ($(this).width() / 50) * 10;
+    //    for (var i = 0; i <= bubblecount; i++) {
+    //      var size = ($.rnd(40, 80) / 10);
+    //      $(this).append('<span class="particle" style="top:' + $.rnd(20, 80) + '%; left:' + $.rnd(0, 95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0, 30) / 10) + 's;"></span>');
+    //    }
+    //  });
+    //}
+
+
+    function confetti() {
+
+      $.each($(".particletext .confetti, .particletext .confetti-r"), function () {
+        var confetticount = 4;
+        for (var i = 0; i <= confetticount; i++) {
+          $(this).append('<span class="particle c' + $.rnd(1, 2) + '" style="top:' + $.rnd(10, 50) + '%; left:' + $.rnd(0, 100) + '%;width:' + $.rnd(6, 8) + 'px; height:' + $.rnd(3, 4) + 'px;animation-delay: ' + ($.rnd(0, 30) / 10) + 's;"></span>');
+        }
+      });
+    }
+
+    $.rnd = function (m, n) {
+      m = parseInt(m);
+      n = parseInt(n);
+      return Math.floor(Math.random() * (n - m + 1)) + m;
+    }
+
+    initparticles();
+
   }
 
 }
