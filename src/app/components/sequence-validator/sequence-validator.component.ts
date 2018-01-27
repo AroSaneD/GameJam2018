6e74f695c0d9879ce4e120d9a1c7f02b5f256c9f
AroSaneD <mail.junk.3135@gmail.com>
2018-01-28 00:32:11 +0200

    started sequence validation logic

--- src/app/components/sequence-validator/sequence-validator.component.ts
+++ src/app/components/sequence-validator/sequence-validator.component.ts
@@ -1,15 +1,32 @@
+import { Card } from './../../model/card';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-sequence-validator',
   templateUrl: './sequence-validator.component.html',
   styleUrls: ['./sequence-validator.component.css']
 })
-export class SequenceValidatorComponent implements OnInit {
+export class SequenceValidatorComponent{
+
+  validationSubject: Subject<boolean>;
+  sequenceToValidate: Card[];
 
   constructor() { }
 
-  ngOnInit() {
+
+  public validateSequnce(cards: Card[]): Observable<boolean>{
+    this.validationSubject = new Subject();
+    this.validationSubject.next(false);
+
+    this.startValidationIntroduction(cards);
+
+    return this.validationSubject;
+  }
+
+  public startValidationIntroduction(cards: Card[]){
+    this.sequenceToValidate = cards;
   }
 
 }
